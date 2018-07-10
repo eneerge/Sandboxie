59813c19d840195728853b38f63d1ff30a5410b3
eneerge <eneerge@eneerge.net>
2018-07-09 22:48:58 -0500

    Faster install and fixed few minor bugs
    Faster install and fixed few minor bugs

--- Install.cmd
+++ Install.cmd
@@ -3,7 +3,7 @@
 :-------------------------------------
 mkdir "%windir%\BatchGotAdmin"
 if '%errorlevel%' == '0' (
-  rmdir "%windir%\BatchGotAdmin" & goto gotAdmin 
+  rmdir "%windir%\BatchGotAdmin" & goto gotAdmin
 ) else ( goto UACPrompt )
 
 :UACPrompt
@@ -15,7 +15,7 @@ if '%errorlevel%' == '0' (
 
 :gotAdmin
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"      
+    pushd "%CD%"
     CD /D "%~dp0""
 :--------------------------------------
 echo Sandboxie Installer Started
@@ -71,28 +71,30 @@ set appIconNumber=0
 ::==========================================
 :: [ Terminate Running Sandbox Application ]
 ::==========================================
-start "" "%sandboxieDir%\start.exe" /box:%sandboxName% /terminate
-if %sandboxToRunIn% neq %sandboxName (
-  start "" "%sandboxieDir%\start.exe" /box:%sandboxToRunIn% /terminate
+if exist "%sandboxDir%\%sandboxName%" (
+  echo - Sandbox already exists, terminating all apps running in sandbox
+  start "" /wait "%sandboxieDir%\start.exe" /box:%sandboxName% /terminate
+  echo - Deleting sandbox
+  start "" /wait "%sandboxieDir%\start.exe" /box:%sandboxName% delete_sandbox
 )
-timeout 1
 
-start "" "%sandboxieDir%\start.exe" /box:%sandboxName% delete_sandbox
-timeout 2
-if %sandboxToRunIn% neq %sandboxName (
-  start "" "%sandboxieDir%\start.exe" /box:%sandboxToRunIn% delete_sandbox
-  timeout 1
+echo - Checking sandbox to run in
+
+if %sandboxToRunIn% neq %sandboxName% (
+  echo - Sandbox to run in different than normal Sandbox
+  if exist "%sandboxDir%\%sandboxToRunIn%" (
+    echo - Sandbox to run in already exists, terminating all apps running in sandbox
+    start "" /wait "%sandboxieDir%\start.exe" /box:%sandboxToRunIn% /terminate
+    timeout 3
+    echo - Deleting sandbox to run in
+    start "" /wait "%sandboxieDir%\start.exe" /box:%sandboxToRunIn% delete_sandbox
+  )
 )
 
 ::==========================================
 :: [ Install Application ]
 ::==========================================
 echo - Copying template sandbox to live sandbox directory
-if %sandboxToRunIn% neq %sandboxName (
-  rd /S /q %sandboxDir%\%sandboxToRunIn%
-)
-rd /S /q %sandboxDir%\%sandboxName%
-md /S /q %sandboxDir%\%sandboxName%
 robocopy "%cd%\%sandboxName%" %sandboxDir%\%sandboxName% /E /W:5
 
 if %sandboxToRunIn% neq %sandboxName (
@@ -108,6 +110,9 @@ call :CreateStartMenuShortcut
 
 echo - Creating Sandboxie Configuration Entries For %sandboxName%
 call :CreateSandbox
+
+echo - Reloading Sandboxie Configuration
+call :ReloadConfig
 
 ::==========================================
 :: [ Exit Installer ]
@@ -136,15 +141,15 @@ goto:eof
 	(
 		echo sLinkFile = "%userprofile%\Desktop\%appName%.lnk"
 		echo Set oLink = oWS.CreateShortcut^(sLinkFile^)
-		echo oLink.TargetPath = "%appExePath%"        
+		echo oLink.TargetPath = "%appExePath%"
 		echo oLink.IconLocation = "%appIconPath%"
 		echo oLink.Save
 	) >> "%temp%\CreateShortcut.vbs"
 
 	rem Extras
-	rem oLink.WindowStyle = "1"   
+	rem oLink.WindowStyle = "1"
 	rem oLink.WorkingDirectory = "C:\Program Files\MyApp"
-	rem oLink.Description = "MyProgram"   
+	rem oLink.Description = "MyProgram"
 	rem oLink.HotKey = "ALT+CTRL+F"
 	rem echo oLink.Arguments = "/box:%sandboxToRunIn% %appExePath%"
 
@@ -173,33 +178,23 @@ goto:eof
 
 :CreateSandbox
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% Enabled y
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% OpenPipePath=Reflector.exe,\Device\NamedPipe\SandboxieReflectorCommands
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% ConfigLevel=7
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% AutoRecover=y
+	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% BlockNetworkFiles=y
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% Template=WindowsFontCache
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% Template=BlockPorts
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% Template=LingerPrograms
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% Template=AutoRecoverIgnore
+	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% RecoverFolder=%{374DE290-123F-4565-9164-39C4925E467B}%
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% RecoverFolder=%Personal%
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% RecoverFolder=%Personal%\Downloads
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% RecoverFolder=%Favorites%
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% RecoverFolder=%Desktop%
 	start "" "%sandboxieDir%\sbieini.exe" set %sandboxName% BorderColor=#00FFFF,ttl
 
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% Enabled y
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% OpenPipePath=Reflector.exe,\Device\NamedPipe\SandboxieReflectorCommands
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% ConfigLevel=7
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% AutoRecover=y
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% Template=WindowsFontCache
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% Template=BlockPorts
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% Template=LingerPrograms
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% Template=AutoRecoverIgnore
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% RecoverFolder=%Personal%
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% RecoverFolder=%Personal%\Downloads
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% RecoverFolder=%Favorites%
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% RecoverFolder=%Desktop%
-	start "" "%sandboxieDir%\sbieini.exe" set %sandboxToRunIn% BorderColor=#00FFFF,ttl
-
-
 	start "" "%sandboxieDir%\start.exe" /reload
+goto:eof
+
+:ReloadConfig
+  start "" "%sandboxieDir%\start.exe" /reload
 goto:eof
